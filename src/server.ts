b07fc4835039296cd43edc9c50b2032207e0d15d
Probir Ghosh <probirghosh@outlook.com>
2025-08-20 20:29:28 +0600

    :tada: app initialised

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import { Server } from "http";
-import express from "express";
 import mongoose from "mongoose";
+import app from "./app";
 
 let server: Server;
-const app = express();
+
 
 const startServer = async () => {
   try {
@@ -22,3 +22,6 @@ const startServer = async () => {
 };
 
 startServer()
+
+
+
